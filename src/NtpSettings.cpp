3a156e21aa350c19417d569f3d7688dda81c5f00
jstammi <jstammi@gmx.net>
2023-08-01 13:16:56 +0200

    Add ntp time sync logging

--- src/NtpSettings.cpp
+++ src/NtpSettings.cpp
@@ -2,9 +2,12 @@
 /*
  * Copyright (C) 2022 Thomas Basler and others
  */
+#include "MessageOutput.h"
 #include "NtpSettings.h"
 #include "Configuration.h"
 #include <time.h>
+#include <sntp.h>
+
 
 NtpSettingsClass::NtpSettingsClass()
 {
@@ -12,6 +15,13 @@ NtpSettingsClass::NtpSettingsClass()
 
 void NtpSettingsClass::init()
 {
+    struct tm now;
+    getLocalTime(&now);
+    MessageOutput.printf("RTC before NTP init: %s\r\n", asctime(&now));
+
+    using std::placeholders::_1;
+    sntp_set_time_sync_notification_cb(&onTimeSync);
+    sntp_set_sync_interval(1800000); // every 30 minutes, default: 1h
     setServer();
     setTimezone();
 }
@@ -25,6 +35,11 @@ void NtpSettingsClass::setTimezone()
 {
     setenv("TZ", Configuration.get().Ntp_Timezone, 1);
     tzset();
+}
+
+void NtpSettingsClass::onTimeSync(timeval *tv)
+{
+  MessageOutput.printf("NTP time sync: %s\r\n", ctime(&tv->tv_sec));
 }
 
 NtpSettingsClass NtpSettings;
